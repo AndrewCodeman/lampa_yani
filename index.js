ba7d6fb2fcd8272c286ea31153f2f68e36074dd2
Andrei Rasskazov <andrei@rasskazoff.com>
2026-08-07 02:29:18 +1200

    Bump plugin version to 0.12.4

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function pluginYummyAnime() {
     if (window.Lampa && Lampa.Manifest) {
         Lampa.Manifest.plugins = {
             type: 'other',
-            version: '0.12.3',
+            version: '0.12.4',
             name: 'YummyAnime',
             description: 'YummyAnime catalog, ratings, lists and account integration',
             component: 'yani_home'
